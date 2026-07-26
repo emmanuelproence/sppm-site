ccef9121688f263aacc0ad2f5e92737994346ff9
emmanuelproence <159497379+emmanuelproence@users.noreply.github.com>
2026-07-26 14:34:36 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// ATENÇÃO: Este arquivo NÃO deve ir para o GitHub!
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBPwg63RIXP6xgasmQEPEemyBLS4eiPe48",
+    authDomain: "sppm-6a369.firebaseapp.com",
+    databaseURL: "https://sppm-6a369-default-rtdb.firebaseio.com",
+    projectId: "sppm-6a369",
+    storageBucket: "sppm-6a369.firebasestorage.app",
+    messagingSenderId: "790068123319",
+    appId: "1:790068123319:web:0607fba6b316a24cad5f32",
+    measurementId: "G-9N8LY8N9G7"
+};
