28b14428d3f46af047733cf52d888b69e651f885
emmanuelproence <159497379+emmanuelproence@users.noreply.github.com>
2026-07-29 18:45:28 -0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,9 @@
-// app/js/app.js
-
-// 1. Importa os módulos essenciais
 import { initAuth } from './auth.js';
 import './ui.js'; 
 
-// 2. Inicia o Motor de Segurança assim que a página carrega
 document.addEventListener('DOMContentLoaded', () => {
-    
-    // Inicia a verificação de Login
     initAuth();
 
-    // Remove qualquer tela de "Carregando" infinita, caso exista no seu HTML
     const loadingScreen = document.getElementById('loading-screen');
     if (loadingScreen) {
         loadingScreen.style.display = 'none';
