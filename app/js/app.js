23a9cd2a63506b45405cc397a6fec336e6161738
emmanuelproence <159497379+emmanuelproence@users.noreply.github.com>
2026-08-04 08:39:03 -0300

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,25 @@
 // app/js/app.js
-
 import { initAuth } from './auth.js';
 import './ui.js'; 
+import { initDashboard } from './dashboard.js';
+import { initLocalFallback, syncCloudToLocal } from './firebase.js';
+import { syncAPI } from './telemetry.js';
 
-document.addEventListener('DOMContentLoaded', () => {
-    // 1. Inicia o Motor de Segurança
+document.addEventListener('DOMContentLoaded', async () => {
+    // 1. Prepara o banco local/nuvem
+    initLocalFallback();
+    await syncCloudToLocal();
+
+    // 2. Inicia o Motor de Segurança
     initAuth();
 
-    // 2. Desliga a tela de "Sincronizando com a Nuvem..."
+    // 3. Inicia os Gráficos e o Mapa
+    initDashboard();
+
+    // 4. Executa a primeira leitura da API climática
+    await syncAPI();
+
+    // 5. Desliga a tela de "Sincronizando com a Nuvem..."
     const loadingOverlay = document.getElementById('loading-overlay');
     if (loadingOverlay) {
         loadingOverlay.style.display = 'none';
